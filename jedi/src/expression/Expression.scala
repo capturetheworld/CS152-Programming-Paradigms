7fd0216bef35cb43f8f02a6890f4eb91c340edef
Ian <ian@iansoohoo.me>
2020-05-10 16:32:59 -0700

    rm hmm

--- jedi/src/expression/Expression.scala
+++ jedi/src/expression/Expression.scala
@@ -5,6 +5,7 @@ import value.Value
 import context.Environment
 
 //questions where values are answers
+//all things inside expression are case classes
 
 trait Expression {
   def execute(env: Environment): Value
--- jedi/src/expression/Iteration.scala
+++ jedi/src/expression/Iteration.scala
@@ -2,11 +2,14 @@ package expression
 import value._
 import expression._
 import context._
+//while (condition) body
+//iteration ::= "while" ~@ "(" ~ expression ~ ")" ~ expression
 
 case class Iteration(condition: Expression, body:Expression) extends SpecialForm{
   override def execute(env: Environment): Value = {
     if (!condition.execute(env).isInstanceOf[Boole]) throw new TypeException("Condition needs to be a boolean. That is a true/false condition to iterate")
     var exit = condition.execute(env).asInstanceOf[Boole].value
+    //like while in scala
     while (exit) {
       body.execute(env)
       exit = condition.execute(env).asInstanceOf[Boole].value
